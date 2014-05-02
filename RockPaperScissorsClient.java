06f544b35412116555040fbf9d3b0f01d895638e
Tom <tomg555@gmail.com>
2014-05-02 11:27:03 -0500

    Added comments, changed from string to char

--- RockPaperScissorsClient.java
+++ RockPaperScissorsClient.java
@@ -1,4 +1,13 @@
 public interface RockPaperScissorsClient{
-	String getNextMove();
-	void sendResult(string result);
+	/**
+	 * char - the move the player choses. Valid moves are:
+	 * 'r', 'p' and 's'
+	 */
+	char getNextMove(); 
+	/**
+	 * Lets the player know if they won, lost or tied
+	 * @param result - valid results are:
+	 * 'w','l', and 't'
+	 */
+	void sendResult(char result); //
 }
