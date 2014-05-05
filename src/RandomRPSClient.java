773fad27f08fe61ef0993c4fd2366e24ecd8700b
Tom <tomg555@gmail.com>
2014-05-05 11:02:27 -0500

    Cleaned up Random client and added stats

--- src/RandomRPSClient.java
+++ src/RandomRPSClient.java
@@ -1,49 +1,54 @@
-import java.util.ArrayList;
+import java.text.NumberFormat;
 import java.util.Random;
 public class RandomRPSClient implements RPSClient
 {
-	//private ArrayList<char> myPreviousMoves;
-	//private ArrayList<char> opponentPreviousMoves;
-	private int wins,loss,tie;
 	private char lastMove;
+	private int rocks,papers,scissors;
+	private Random rand;
+	
 	public RandomRPSClient()
 	{
-		//myPreviousMoves = new ArrayList<char>();
-		//opponentPreviousMoves = new ArrayList<char>();
-		wins=0;
-		loss=0;
-		tie=0;
+		rand = new Random();
+		rocks=0;
+		papers=0;
+		scissors=0;
 	}
 	public char nextMove()
 	{
-		Random rand = new Random();
 		int r = rand.nextInt(3);
-		
 		if(r<1){
 			lastMove=ValidMoves.Rock;
+			rocks++;
 		}
 		else if(r<2){
 			lastMove=ValidMoves.Paper;
+			papers++;
 		}
 		else if(r<3){
 			lastMove=ValidMoves.Scissors;
+			scissors++;
 		}
 		else{
 			lastMove='x';
 		}
 		return lastMove;
-			
 	}
 	public void result(char result)
 	{
-		if(result==GameStates.Win){
-			wins++;
-		}
-		else if(result==GameStates.Lose){
-			loss++;
-		}
-		else if(result==GameStates.Tie){
-			tie++;
-		}
+	}
+	
+	public void printStats(){
+		NumberFormat percentFormat = NumberFormat.getPercentInstance();
+		double percent = 0;
+		int sum = rocks+papers+scissors;
+		
+		percent=(double)rocks/sum;
+		System.out.println("Rocks: "+percentFormat.format(percent));
+		
+		percent=(double)papers/sum;
+		System.out.println("Papers: "+percentFormat.format(percent));
+		
+		percent=(double)scissors/sum;
+		System.out.println("Scissors: "+percentFormat.format(percent));
 	}
 }
