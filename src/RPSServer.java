a6430bc3ba4547ea4c5167c1582e3e2bbb172bde
Tom <tomg555@gmail.com>
2014-05-05 23:37:19 -0500

    updated TomRPSClient

--- src/RPSServer.java
+++ src/RPSServer.java
@@ -3,8 +3,8 @@ public class RPSServer
 	public static void main(String[] args)
 	{
 		RPSClient player1 = new RandomRPSClient();
-		RPSClient player2 = new HumanRPSClient();
-		int NumberOfRounds = 5;//Integer.parseInt(args[0]);
+		RPSClient player2 = new TomRPSClient();
+		int NumberOfRounds = 5000;//Integer.parseInt(args[0]);
 		int winner;
 		GameRecord game = new GameRecord();
 		for(int i=0;i<NumberOfRounds;i++)
